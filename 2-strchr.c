30fd273ad110052abf9848432983c0f9adf29609
Henock Teshome Bekele <118671506+heni44@users.noreply.github.com>
2023-01-02 07:22:53 -0800

    Create 2-strchr.c

---
+++ 2-strchr.c
@@ -0,0 +1,26 @@
+julien@ubuntu:~/0x07$ cat 2-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "hello";
+    char *f;
+
+    f = _strchr(s, 'l');
+
+    if (f != NULL)
+    {
+        printf("%s\n", f);
+    }
+    return (0);
+}
+julien@ubuntu:~/0x07$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-strchr.c -o 2-strchr
+julien@ubuntu:~/0x07$ ./2-strchr 
+llo
+julien@ubuntu:~/0x07$
