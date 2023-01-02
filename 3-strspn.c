8241791a43ef8267193be34ade0e6f4a976b2f0b
Henock Teshome Bekele <118671506+heni44@users.noreply.github.com>
2023-01-02 07:24:44 -0800

    Create 3-strspn.c

---
+++ 3-strspn.c
@@ -0,0 +1,23 @@
+julien@ubuntu:~/0x07$ cat 3-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "hello, world";
+    char *f = "oleh";
+    unsigned int n;
+
+    n = _strspn(s, f);
+    printf("%u\n", n);
+    return (0);
+}
+julien@ubuntu:~/0x07$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 3-strspn.c -o 3-strspn
+julien@ubuntu:~/0x07$ ./3-strspn 
+5
+julien@ubuntu:~/0x07$ 
