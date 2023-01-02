a042e882156b48e765c48121a57a494d2bf5a913
Henock Teshome Bekele <118671506+heni44@users.noreply.github.com>
2023-01-02 07:27:35 -0800

    Create 4-strpbrk.c

---
+++ 4-strpbrk.c
@@ -0,0 +1,23 @@
+julien@ubuntu:~/0x07$ cat 4-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "hello, world";
+    char *f = "world";
+    char *t;
+
+    t = _strpbrk(s, f);
+    printf("%s\n", t);
+    return (0);
+}
+julien@ubuntu:~/0x07$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 4-main.c 4-strpbrk.c -o 4-strpbrk
+julien@ubuntu:~/0x07$ ./4-strpbrk 
+llo, world
+julien@ubuntu:~/0x07$
