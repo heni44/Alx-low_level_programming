45222fad9af8be3043a6944fe26076be7f21b72e
Henock Teshome Bekele <118671506+heni44@users.noreply.github.com>
2023-01-04 04:04:38 -0800

    Delete 0-puts_recursion.c

--- 0-puts_recursion.c
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env perl
-# (c) 2001, Dave Jones. (the file handling bit)
-# (c) 2005, Joel Schopp <jschopp@austin.ibm.com> (the ugly bit)
-# (c) 2007,2008, Andy Whitcroft <apw@uk.ibm.com> (new conditions, test suite)
-# (c) 2008-2010 Andy Whitcroft <apw@canonical.com>
-# Licensed under the terms of the GNU GPL License version 2
