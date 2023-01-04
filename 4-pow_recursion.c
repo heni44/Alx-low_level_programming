d6a4c418ddc9b9ff8b96b832208f4796534dad56
Henock Teshome Bekele <118671506+heni44@users.noreply.github.com>
2023-01-04 05:40:20 -0800

    Update 4-pow_recursion.c

--- 4-pow_recursion.c
+++ 4-pow_recursion.c
@@ -1,4 +1,4 @@
-julien@ubuntu:~/0x08. Recursion cat 4-main.c
+julien@ubuntu:~/0x08. Recursion$ cat 4-main.c
 #include "main.h"
 #include <stdio.h>
 
@@ -9,28 +9,28 @@ julien@ubuntu:~/0x08. Recursion cat 4-main.c
  */
 int main(void)
 {
-    int r
+    int r;
 
-    r = _pow_recursion(1, 10)
-    printf("%d\n", r)
-    r = _pow_recursion(1024, 0)
+    r = _pow_recursion(1, 10);
     printf("%d\n", r);
-    r = _pow_recursion(2, 16)
+    r = _pow_recursion(1024, 0);
     printf("%d\n", r);
-    r = _pow_recursion(5, 2)
+    r = _pow_recursion(2, 16);
     printf("%d\n", r);
-    r = _pow_recursion(5, -2)
+    r = _pow_recursion(5, 2);
     printf("%d\n", r);
-    r = _pow_recursion(-5, 3)
-    printf("%d\n", r)
-    return (0)
+    r = _pow_recursion(5, -2);
+    printf("%d\n", r);
+    r = _pow_recursion(-5, 3);
+    printf("%d\n", r);
+    return (0);
 }
-julien@ubuntu:~/0x08. Recursion gcc -Wall -pedantic -Werror -Wextra -std=gnu89 4-main.c 4-pow_recursion.c -o 4-pow
-julien@ubuntu:~/0x08. Recursion ./4-pow 
+julien@ubuntu:~/0x08. Recursion$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 4-main.c 4-pow_recursion.c -o 4-pow
+julien@ubuntu:~/0x08. Recursion$ ./4-pow 
 1
 1
 65536
 25
 -1
 -125
-julien@ubuntu:~/0x08. Recursion
+julien@ubuntu:~/0x08. Recursion$
