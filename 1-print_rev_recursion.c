f63e1dcb0d0149d474a7dd2b1a43f87d71a21a36
Henock Teshome Bekele <118671506+heni44@users.noreply.github.com>
2023-01-04 05:44:26 -0800

    Update 1-print_rev_recursion.c

--- 1-print_rev_recursion.c
+++ 1-print_rev_recursion.c
@@ -1,15 +1,17 @@
-void print(int nb)
-{
-    printf("%d", nb);
-    -- nb;
-    if (nb > 0) 
-    {
-        print(nb);
-    }
-}
+julien@ubuntu:~/0x08. Recursion$ cat 1-main.c
+#include "main.h"
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    print(4);
+    _print_rev_recursion("\nColton Walker");
     return (0);
 }
+julien@ubuntu:~/0x08. Recursion$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 1-main.c 1-print_rev_recursion.c -o 1-print_rev_recursion
+julien@ubuntu:~/0x08. Recursion$ ./1-print_rev_recursion 
+reklaW notloC
+julien@ubuntu:~/0x08. Recursion$
