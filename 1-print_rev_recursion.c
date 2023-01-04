0c696602dea83066f309552127a4e7540fecc630
Henock Teshome Bekele <118671506+heni44@users.noreply.github.com>
2023-01-04 04:42:25 -0800

    Update 1-print_rev_recursion.c

--- 1-print_rev_recursion.c
+++ 1-print_rev_recursion.c
@@ -1,21 +1,5 @@
-julien@ubuntu:~/0x08. Recursion$ cat 2-main.c 
-#include "main.h"
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int n;
-
-    n = _strlen_recursion("Corbin Coleman");
-    printf("%d\n", n);
-    return (0);
-}
-julien@ubuntu:~/0x08. Recursion$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89  2-main.c 2-strlen_recursion.c -o 2-strlen_recursion
-julien@ubuntu:~/0x08. Recursion$ ./2-strlen_recursion 
-14
-julien@ubuntu:~/0x08. Recursion$
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
