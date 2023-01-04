dfb0ad83272db534c2278cb2cc1ea383c872b76f
Henock Teshome Bekele <118671506+heni44@users.noreply.github.com>
2023-01-04 06:23:23 -0800

    Update 1-print_rev_recursion.c

--- 1-print_rev_recursion.c
+++ 1-print_rev_recursion.c
@@ -1,12 +1,4 @@
 julien@ubuntu:~/0x08. Recursion$ cat 1-main.c
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
 {
     _print_rev_recursion("\nColton Walker");
     return (0);
