d21423969e7a6288b5c3e400079d07f9770ef75b
Henock Teshome Bekele <118671506+heni44@users.noreply.github.com>
2023-01-04 05:30:42 -0800

    Update 101-wildcmp.c

--- 101-wildcmp.c
+++ 101-wildcmp.c
@@ -1,11 +1,55 @@
+
   julien@ubuntu:~/0x08. Recursion$ cat 101-main.c
-   /**return:1
-/*return:2
-_putchar.c
+#include "main.h"
+#include <stdio.h>
+
 /**
- * main - check the code
+ * main - check the codec
  *
  * Return: Always 0.
  */
-int main(void)                                                    
-     
+int main(void)
+{
+    int r;
+
+    r = wildcmp("main.c", "*.c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m*a*i*n*.*c*");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "main.c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m*c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "ma********************************c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "*");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "***");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m.*c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "**.*c");
+    printf("%d\n", r);
+    r = wildcmp("main-main.c", "ma*in.c");
+    printf("%d\n", r);
+    r = wildcmp("main", "main*d");
+    printf("%d\n", r);
+    r = wildcmp("abc", "*b");
+    printf("%d\n", r);
+    return (0);
+}
+julien@ubuntu:~/0x08. Recursion$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 101-main.c 101-wildcmp.c -o 101-wildcmp
+julien@ubuntu:~/0x08. Recursion$ ./101-wildcmp 
+1
+1
+1
+1
+1
+1
+1
+0
+1
+1
+0
+0
+julien@ubuntu:~/0x08. Recursion$  
