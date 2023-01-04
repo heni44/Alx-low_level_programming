42d5c482c52c7b6f507a8d510d33c4bba9216fe0
Henock Teshome Bekele <118671506+heni44@users.noreply.github.com>
2023-01-04 04:48:26 -0800

    Update 2-strlen_recursion.c

--- 2-strlen_recursion.c
+++ 2-strlen_recursion.c
@@ -1,5 +1,12 @@
 int_strlen_recursion(char *s)
+{
+    int n;
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+    n = _strlen_recursion("Corbin Coleman");
+    printf("%d\n", n);
+    return (0);
+}
+julien@ubuntu:~/0x08. Recursion$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89  2-main.c 2-strlen_recursion.c -o 2-strlen_recursion
+julien@ubuntu:~/0x08. Recursion$ ./2-strlen_recursion 
+14
+julien@ubuntu:~/0x08. Recursion$
