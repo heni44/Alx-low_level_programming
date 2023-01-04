1ae7346865fbd4dda80a434428af1b875fd4a9df
Henock Teshome Bekele <118671506+heni44@users.noreply.github.com>
2023-01-04 05:38:18 -0800

    Update 5-sqrt_recursion.c

--- 5-sqrt_recursion.c
+++ 5-sqrt_recursion.c
@@ -1,8 +1,36 @@
-julien@ubuntu:~/0x08. Recursion ./5-sqrt 
+julien@ubuntu:~/0x08. Recursion$ cat 5-main.c 
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = _sqrt_recursion(1);
+    printf("%d\n", r);
+    r = _sqrt_recursion(1024);
+    printf("%d\n", r);
+    r = _sqrt_recursion(16);
+    printf("%d\n", r);
+    r = _sqrt_recursion(17);
+    printf("%d\n", r);
+    r = _sqrt_recursion(25);
+    printf("%d\n", r);
+    r = _sqrt_recursion(-1);
+    printf("%d\n", r);
+    return (0);
+}
+julien@ubuntu:~/0x08. gcc -Wall -pedantic -Werror -Wextra -std=gnu89 5-main.c 5-sqrt_recursion.c -o 5-sqrt
+julien@ubuntu:~/0x08. Recursion$ ./5-sqrt 
 1
 32
 4
 -1
 5
 -1
-julien@ubuntu:~/0x08. Recursion.c
+julien@ubuntu:~/0x08. Recursion$ 
