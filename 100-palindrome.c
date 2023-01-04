0d6ecbc263563ef7fa3ca4da39152fc791a71f7f
Henock Teshome Bekele <118671506+heni44@users.noreply.github.com>
2023-01-04 05:35:16 -0800

    Update and rename 100-ipalindrome.c to 100-palindrome.c

---
+++ 100-palindrome.c
@@ -0,0 +1,30 @@
+julien@ubuntu:~/0x08. Recursion$ cat 100-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = is_palindrome("level");
+    printf("%d\n", r);
+    r = is_palindrome("redder");
+    printf("%d\n", r);
+    r = is_palindrome("test");
+    printf("%d\n", r);
+    r = is_palindrome("step on no pets");
+    printf("%d\n", r);
+    return (0);
+}
+julien@ubuntu:~/0x08. Recursion$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 100-main.c 100-is_palindrome.c -o 100-palindrome
+julien@ubuntu:~/0x08. Recursion$ ./100-palindrome 
+1
+1
+0
+1
+julien@ubuntu:~/0x08. Recursion$
