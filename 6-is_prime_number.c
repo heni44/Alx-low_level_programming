9f5a5d97dbbe98c562fd058c522fd41149d4dd63
Henock Teshome Bekele <118671506+heni44@users.noreply.github.com>
2023-01-04 05:36:39 -0800

    Update 6-is_prime_number.c

--- 6-is_prime_number.c
+++ 6-is_prime_number.c
@@ -1,5 +1,36 @@
-int is_prime_number(int n)
-julien@ubuntu:~/0x08. Recursion ./6-prime 
+julien@ubuntu:~/0x08. Recursion$ cat 6-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = is_prime_number(1);
+    printf("%d\n", r);
+    r = is_prime_number(1024);
+    printf("%d\n", r);
+    r = is_prime_number(16);
+    printf("%d\n", r);
+    r = is_prime_number(17);
+    printf("%d\n", r);
+    r = is_prime_number(25);
+    printf("%d\n", r);
+    r = is_prime_number(-1);
+    printf("%d\n", r);
+    r = is_prime_number(113);
+    printf("%d\n", r);
+    r = is_prime_number(7919);
+    printf("%d\n", r);
+    return (0);
+}
+julien@ubuntu:~/0x08. Recursion$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 6-main.c 6-is_prime_number.c -o 6-prime
+julien@ubuntu:~/0x08. Recursion$ ./6-prime 
 0
 0
 0
@@ -8,4 +39,4 @@ julien@ubuntu:~/0x08. Recursion ./6-prime
 0
 1
 1
-julien@ubuntu:~/0x08. Recursion.c
+julien@ubuntu:~/0x08. Recursion$
