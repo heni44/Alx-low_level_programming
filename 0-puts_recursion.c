c0dfaad5700af7418322d548b2687e73f86554ce
Henock Teshome Bekele <118671506+heni44@users.noreply.github.com>
2023-01-04 06:20:52 -0800

    Update 0-puts_recursion.c

--- 0-puts_recursion.c
+++ 0-puts_recursion.c
@@ -1,17 +1,6 @@
-julien@ubuntu:~/0x08. Recursion$ cat 0-main.c
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    _puts_recursion("Puts with recursion");
-    return (0);
-}
-julien@ubuntu:~/0x08. Recursion$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 0-main.c 0-puts_recursion.c -o 0-puts_recursion
-julien@ubuntu:~/0x08. Recursion$ ./0-puts_recursion 
-Puts with recursion
-julien@ubuntu:~/0x08. Recursion$ 
+#!/usr/bin/env perl
+# (c) 2001, Dave Jones. (the file handling bit)
+# (c) 2005, Joel Schopp <jschopp@austin.ibm.com> (the ugly bit)
+# (c) 2007,2008, Andy Whitcroft <apw@uk.ibm.com> (new conditions, test suite)
+# (c) 2008-2010 Andy Whitcroft <apw@canonical.com>
+# Licensed under the terms of the GNU GPL License version 2
