922d83696e49a82e42eaf3d33762ca56a383cc8a
Henock Teshome Bekele <118671506+heni44@users.noreply.github.com>
2023-01-04 05:42:43 -0800

    Update 2-strlen_recursion.c

--- 2-strlen_recursion.c
+++ 2-strlen_recursion.c
@@ -1,4 +1,13 @@
-int_strlen_recursion(char *s)
+julien@ubuntu:~/0x08. Recursion$ cat 2-main.c 
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
     int n;
 
@@ -9,4 +18,4 @@ int_strlen_recursion(char *s)
 julien@ubuntu:~/0x08. Recursion$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89  2-main.c 2-strlen_recursion.c -o 2-strlen_recursion
 julien@ubuntu:~/0x08. Recursion$ ./2-strlen_recursion 
 14
-julien@ubuntu:~/0x08. Recursion$
+julien@ubuntu:~/0x08. Recursion$ 
