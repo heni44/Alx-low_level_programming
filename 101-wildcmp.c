3f99eaee985e403cf77a5c0dfa423d347cf64a54
Henock Teshome Bekele <118671506+heni44@users.noreply.github.com>
2023-01-04 05:28:02 -0800

    Create 101-wildcmp.c

---
+++ 101-wildcmp.c
@@ -0,0 +1,11 @@
+  julien@ubuntu:~/0x08. Recursion$ cat 101-main.c
+   /**return:1
+/*return:2
+_putchar.c
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)                                                    
+     
