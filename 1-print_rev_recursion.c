487e2614696690e1ada30e548aed48a66401efc2
Henock Teshome Bekele <118671506+heni44@users.noreply.github.com>
2023-01-04 04:38:48 -0800

    Create 1-print_rev_recursion.c

---
+++ 1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+julien@ubuntu:~/0x08. Recursion$ cat 2-main.c 
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
+
+    n = _strlen_recursion("Corbin Coleman");
+    printf("%d\n", n);
+    return (0);
+}
+julien@ubuntu:~/0x08. Recursion$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89  2-main.c 2-strlen_recursion.c -o 2-strlen_recursion
+julien@ubuntu:~/0x08. Recursion$ ./2-strlen_recursion 
+14
+julien@ubuntu:~/0x08. Recursion$
